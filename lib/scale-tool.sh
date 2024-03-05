2fdbf076e03d596d788a59541931dd66d19dd8ec
gqlo <guoqing_li@pm.me>
2024-03-05 06:34:55 +0000

    add timestamp extraction

--- lib/scale-tool.sh
+++ lib/scale-tool.sh
@@ -1,9 +1,3 @@
-# file paths
-vm_template="/root/h-bench/template/cnv/vm.yaml"
-vm_deployment_path="/root/cnv/data/batch_completed_time.csv"
-vmi_running_path="/root/cnv/data/vmi_running_time.csv"
-batch_num=1000
-
 # batch create 100 VMs at a time
 batch_create_vm() {
    local vm_num="$1"
@@ -12,12 +6,11 @@ batch_create_vm() {
       sed "s/placeholder/$i/g" "$vm_template" | oc create -f - &
    done
 }
-
 batch_start_vm() {
    local vm_num="$1"
    local i
    for ((i="$vm_num"; i<vm_num+batch_num; i++)); do
-      virtctl start rhel9-$i &
+      virtctl start win11-$i &
    done
 }
 
@@ -39,18 +32,18 @@ wait_dv_clone() {
    local vm_num="$1"
    local start="$vm_num"
    local end=$((vm_num + batch_num))
-   local timeout=$((batch_num*9)) # on average each dv clone takes 9 seconds
+   local timeout=$((batch_num*18)) # on average each dv clone takes 9 seconds
    echo "wait dv clone start=$start, end=$end"
    local current_dv_num=$(count_dv_line "$start" "$end")
    while [[ $current_dv_num -ne $batch_num ]]; do
-     if [[ "$timeout" -lt 5 ]]; then
-         return 1
-     fi
-     timeout=$((timeout - 5 ))
-     echo "wait dv clone start=$start, end=$end"
-     current_dv_num=$(count_dv_line "$start" "$end")
-     echo "current completed dv clone: $current_dv_num, timeout: $timeout/$((batch_num*9))"
-     sleep 5
+      if [[ "$timeout" -lt 5 ]]; then
+          return 1
+      fi
+      timeout=$((timeout - 5 ))
+      echo "wait dv clone start=$start, end=$end"
+      current_dv_num=$(count_dv_line "$start" "$end")
+      echo "current completed dv clone: $current_dv_num, timeout: $timeout/$((batch_num*18))"
+      sleep 5
    done
    return 0
 }
@@ -73,8 +66,41 @@ wait_vm_running() {
    return 0
 }
 
+get_dv_timestamps() {
+   local dvs="$1"
+   for dv in $dvs; do
+      dv_creation_ts=$(oc get dv $dv -o jsonpath='{.metadata.creationTimestamp}')
+      dv_bound_ts=$(oc get dv $dv -o jsonpath='{.status.conditions[?(@.type=="Bound")].lastTransitionTime}')
+      dv_creation_unix=$(date -d "$dv_creation_ts" +"%s")
+      dv_bound_unix=$(date -d "$dv_bound_ts" +"%s")
+      deployment_time=$((dv_bound_unix - dv_creation_unix))
+      echo "$dv, $deployment_time" | tee -a "$vm_deployment_ts" 
+   done
+}
 
-delete_all_vm() {
+get_vmi_boot_time() {
+   local vmis="$1"
+   for vm in $vmis; do
+      Pending=$(oc get vmi $vm -o jsonpath='{.status.phaseTransitionTimestamps[?(@.phase=="Pending")].phaseTransitionTimestamp}')
+      Scheduled=$(oc get vmi $vm -o jsonpath='{.status.phaseTransitionTimestamps[?(@.phase=="Scheduled")].phaseTransitionTimestamp}')
+      Pending_unix_ts=$(date -d "$Pending" +"%s")
+      Scheduled_unix_ts=$(date -d "$Scheduled" +"%s")
+      if [[ $vm =~ [Ww][Ii][Nn] ]]; then
+         timestamp_string=$(virtctl ssh Administrator@$vm --command "type C:\Users\Administrator\timestamp.txt")
+      else
+         timestamp_string=$(virtctl ssh root@$vm --command "cat /root/timestamp.txt" | sed -n 2p)
+      fi
+      boot_ts="${timestamp_string#*,}"
+      boot_unix="${timestamp_string%%,*}"
+      boot_unix_ts=$(date -d "$boot_ts" +"%s")
+      boot_time=$((boot_unix_ts - Scheduled_unix_ts))
+      schedule_time=$((Scheduled_unix_ts - Pending_unix_ts))
+      total_time=$((boot_unix_ts - Pending_unix_ts))
+      echo "$vm, $schedule_time, $boot_time, $total_time" | tee -a  "$vmi_boot_ts"
+   done
+}
+
+delete_vm() {
    oc delete vm --all -n default
 }
 
@@ -93,7 +119,8 @@ clean_odf_disk() {
 sync_clock() {
    local node="$1"
    oc debug node/"$node" -- chroot /host /bin/bash -c \
-     "setenforce 0 && \
+     "systemctl restart chronyd && \
+      setenforce 0 && \
       echo 'setenforce to disable selinux succeeded' || { echo 'setenforce to disable selinux failed on node $node'; exit 1; } && \
       chronyc -a makestep && \
       echo 'chronyc cmd succeeded' || { echo 'chronyc failed on node $node'; exit 1; } && \
@@ -111,12 +138,13 @@ deploy_vm() {
 		wait_dv_clone "$i"
       status=$?
 		local end_time=$(date +%s)
-      if [[ $status eq 1 ]]; then
-         echo "$i-$((i+batch_num-1)), vm deployment timeout: $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$vm_deployment_path"
-      elif [[ $status eq 0 ]]; then
+      if [[ $status -eq 1 ]]; then
+         echo "$i-$((i+batch_num-1)), vm deployment timeout: $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$batch_deployment_ts"
+      elif [[ $status -eq 0 ]]; then
 		   echo "batch number $i, completed in $((end_time - start_time))"
-         echo "$i-$((i+batch_num-1)), $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$vm_deployment_path"
+         echo "win11-$i-$((i+batch_num-1)), $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$batch_deployment_ts"
       fi
+   sleep 30
 	done
 }
 
@@ -130,12 +158,21 @@ start_vm() {
 		wait_vm_running "$i"
       status=$?
       local end_time=$(date +%s)
-      if [[ $status eq 1 ]]; then
-         echo "$i-$((i+batch_num-1)), vmi start timeout: $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$vmi_running_path"
-      elif [[ $status eq 0 ]]; then
+      if [[ $status -eq 1 ]]; then
+         echo "$i-$((i+batch_num-1)), vmi start timeout: $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$vmi_running_ts"
+      elif [[ $status -eq 0 ]]; then
 		   echo "batch number $i, vmi start running in $((end_time - start_time))"
-		   echo "$i-$((i+batch_num-1)), $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$vmi_running_path"
+		   echo "win11-$i-$((i+batch_num-1)), $((end_time - start_time)), $(date -d "@$start_time" +"%Y-%m-%d %H:%M:%S"), $(date -d "@$end_time" +"%Y-%m-%d %H:%M:%S")" | tee -a "$vmi_running_ts"
       fi
+   sleep 30
    done
 }
+
+# file paths
+vm_template="/root/h-bench/template/cnv/win-vm.yaml"
+batch_deployment_ts="/root/cnv/data/batch_completed_time.csv"
+vm_deployment_ts="/root/cnv/data/deployment_time_ts.csv"
+vmi_boot_ts="/root/cnv/data/vmi_boot_ts.csv"
+vmi_running_ts="/root/cnv/data/vmi_running_time.csv"
+batch_num=100
 
