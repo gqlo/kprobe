b0d70c9d2847de7e1c90bb830ea867aef99ae997
gqlo <guoqing_li@pm.me>
2024-03-22 01:23:52 +0000

    include block size label

--- workload/fio.sh
+++ workload/fio.sh
@@ -8,8 +8,9 @@ if [[ ! -d /root/fio-output/ ]]; then
 fi
 
 # workload=(randwrite randread randrw)
-workload=(randread)
-block_size=(4k 8k 16k 32k 64k 128k 256k 1024k)
+workload=(randwrite)
+# block_size=(4k 8k 16k 32k 64k 128k 256k 1024k)
+block_size=(4k)
 iodepth=256
 num_jobs=1
 run_time=30m
@@ -24,19 +25,20 @@ for load in ${workload[@]}; do
       if [[ -f "$output_file" ]]; then
         sudo rm -rf "$output_file"
       fi
-      
+
       if [[ -z "$FIO" ]]; then
          sudo dnf install -y fio
       fi
+      echo "batch=$batch_num, vmi=$hname, fio=$load, blk=$blk, started $(date +"%Y-%m-%d %H:%M:%S")"
 
-      echo "batch=$batch_num, vmi=$hname, fio=$load started $(date +"%Y-%m-%d %H:%M:%S")"
       if [[ $load == "randread" ]]; then
          fio --name=$hname --filename=/dev/vda --ioengine="$io_engine" --rw="$load" --bs="$blk" --direct="$o_direct" --numjobs="$num_jobs" --size="$file_size" --runtime="$run_time"  --iodepth="$iodepth" --output="$output_file"
       else
          fio --name=$hname --ioengine="$io_engine" --rw="$load" --bs="$blk" --direct="$o_direct" --numjobs="$num_jobs" --size="$file_size" --runtime="$run_time"  --iodepth="$iodepth" --output="$output_file"
       fi
+
       sudo rm -rf /root/rhel9*
-      echo "batch=$batch_num, vmi=$hname, fio=$load ended $(date +"%Y-%m-%d %H:%M:%S")"
+      echo "batch=$batch_num, vmi=$hname, fio=$load, blk=$blk, ended $(date +"%Y-%m-%d %H:%M:%S")"
       sleep 15
    done
 done
